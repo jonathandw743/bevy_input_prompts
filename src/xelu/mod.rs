5cbd9e0fbb641e0098ac7c00f82f2a4c2a134f60
jonathandw743 <jonathandw414@gmail.com>
2025-04-01 00:31:39 +0100

    keycodes

--- src/xelu/mod.rs
+++ src/xelu/mod.rs
@@ -1,9 +1,40 @@
-mod xelu_key_code_name;
-mod xelu_mouse_button_name;
-mod xelu_key_code;
-mod xelu_mouse_button;
+use std::{env::join_paths, path::PathBuf};
+
+use bevy_app::{App, Plugin};
+use bevy_asset::{AssetPlugin, embedded_asset};
+use fs_extra::copy_items;
+use fs_extra::dir::CopyOptions;
+use fs_extra::dir::copy;
+
+pub mod xelu_key_code;
+pub mod xelu_key_code_name;
+pub mod xelu_mouse_button;
+pub mod xelu_mouse_button_name;
+
+use std::env;
+use std::fs;
+use walkdir::WalkDir;
 
 pub enum LightDark {
     Light,
     Dark,
+}
+
+pub struct XeluPlugin;
+impl XeluPlugin {
+    const ASSET_DIRS: [&'static str; 1] = ["xelu"];
+}
+
+impl Plugin for XeluPlugin {
+    fn build(&self, _app: &mut App) {
+        let addon_asset_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("assets");
+        let asset_paths = Self::ASSET_DIRS
+            .iter()
+            .map(|dir| addon_asset_dir.join(dir))
+            .collect::<Vec<_>>();
+        let this_asset_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap()).join("assets");
+        fs::create_dir_all(&this_asset_dir).expect("creating asset directory failed");
+        copy_items(&asset_paths, this_asset_dir, &CopyOptions::default())
+            .expect("copying assets failed");
+    }
 }
--- src/xelu/xelu_mouse_button.rs
+++ src/xelu/xelu_mouse_button.rs
@@ -1,28 +1,28 @@
-use bevy_asset::AssetPath;
+use bevy_asset::{Asset, AssetPath, AssetServer, Handle};
 use bevy_input::mouse::MouseButton;
 
 use super::{xelu_mouse_button_name::mouse_button_name, LightDark};
 
 pub struct XeluMouseButton {
-    mouse_button: MouseButton,
-    light_dark: LightDark,
+    pub mouse_button: MouseButton,
+    pub light_dark: LightDark,
 }
 
 impl XeluMouseButton {
-    fn default_path(&self) -> &'static str {
+    pub fn default_path(&self) -> &'static str {
         match self.light_dark {
             LightDark::Dark => {
-                "Xelu_Free_Controller&Key_Prompts/Keyboard & Mouse/Blanks/Blank_Black_Mouse.png"
+                "xelu/Xelu_Free_Controller&Key_Prompts/Keyboard & Mouse/Blanks/Blank_Black_Mouse.png"
             }
             LightDark::Light => {
-                "Xelu_Free_Controller&Key_Prompts/Keyboard & Mouse/Blanks/Blank_White_Mouse.png"
+                "xelu/Xelu_Free_Controller&Key_Prompts/Keyboard & Mouse/Blanks/Blank_White_Mouse.png"
             }
         }
     }
-    fn format_mouse_button_name(&self, key_name: &str) -> String {
+    pub fn format_mouse_button_name(&self, key_name: &str) -> String {
         match self.light_dark {
-            LightDark::Light => format!("Xelu_Free_Controller&Key_Prompts/Keyboard & Mouse/Light/{}_Key_Light.png", key_name),
-            LightDark::Dark => format!("Xelu_Free_Controller&Key_Prompts/Keyboard & Mouse/Dark/{}_Key_Dark.png", key_name),
+            LightDark::Light => format!("xelu/Xelu_Free_Controller&Key_Prompts/Keyboard & Mouse/Light/{}_Key_Light.png", key_name),
+            LightDark::Dark => format!("xelu/Xelu_Free_Controller&Key_Prompts/Keyboard & Mouse/Dark/{}_Key_Dark.png", key_name),
         }
     }
 }
