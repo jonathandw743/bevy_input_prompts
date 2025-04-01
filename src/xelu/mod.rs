e4df98afe65e90c843d8763ebc8b25f107567715
jonathandw743 <jonathandw414@gmail.com>
2025-04-01 02:18:21 +0100

    xbox series and ps5

--- src/xelu/mod.rs
+++ src/xelu/mod.rs
@@ -1,11 +1,19 @@
 pub mod key_code;
 pub mod mouse_button;
+pub mod gamepad_button;
 
 use super::copy_assets;
 
 pub enum LightDark {
     Light,
     Dark,
+}
+
+pub enum GamepadBrand {
+    PS5,
+    SteamDeck,
+    Switch,
+    XboxSeries,
 }
 
 pub const ASSET_DIRS: [&'static str; 1] = ["xelu"];
