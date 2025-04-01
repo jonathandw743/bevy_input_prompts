ad1240ee5e54b13eea8ea10fca4f75bcf0e6153f
jonathandw743 <jonathandw414@gmail.com>
2025-04-01 16:53:55 +0100

    optional inputs in detection example

--- examples/detection.rs
+++ examples/detection.rs
@@ -1,5 +1,7 @@
 use bevy::{math::vec2, prelude::*};
-use bevy_input_prompts::xelu::{gamepad_button::XeluGamepadButton, key_code::XeluKeyCode, GamepadBrand, LightDark};
+use bevy_input_prompts::xelu::{
+    GamepadBrand, LightDark, gamepad_button::XeluGamepadButton, key_code::XeluKeyCode,
+};
 
 fn main() {
     App::new()
@@ -21,32 +23,36 @@ fn update(
     mut commands: Commands,
     show_input: Single<Entity, With<ShowInput>>,
     asset_server: Res<AssetServer>,
-    key_code_input: Res<ButtonInput<KeyCode>>,
-    gamepad_button_input: Res<ButtonInput<GamepadButton>>,
+    key_code_input: Option<Res<ButtonInput<KeyCode>>>,
+    gamepad_button_input: Option<Res<ButtonInput<GamepadButton>>>,
 ) {
     let mut entity = commands.entity(*show_input);
-    if let Some(&key_code) = key_code_input.get_just_pressed().next() {
-        dbg!(key_code);
-        entity.insert(Sprite {
-            image: asset_server.load(XeluKeyCode {
-                key_code,
-                light_dark: LightDark::Dark,
-            }),
-            custom_size: Some(vec2(100.0, 100.0)),
-            ..default()
-        });
-        return;
+    if let Some(key_code_input) = key_code_input {
+        if let Some(&key_code) = key_code_input.get_just_pressed().next() {
+            dbg!(key_code);
+            entity.insert(Sprite {
+                image: asset_server.load(XeluKeyCode {
+                    key_code,
+                    light_dark: LightDark::Dark,
+                }),
+                custom_size: Some(vec2(100.0, 100.0)),
+                ..default()
+            });
+            return;
+        }
     }
-    if let Some(&gamepad_button) = gamepad_button_input.get_just_pressed().next() {
-        dbg!(gamepad_button);
-        entity.insert(Sprite {
-            image: asset_server.load(XeluGamepadButton {
-                gamepad_button,
-                gamepad_brand: GamepadBrand::XboxSeries,
-            }),
-            custom_size: Some(vec2(100.0, 100.0)),
-            ..default()
-        });
-        return
+    if let Some(gamepad_button_input) = gamepad_button_input {
+        if let Some(&gamepad_button) = gamepad_button_input.get_just_pressed().next() {
+            dbg!(gamepad_button);
+            entity.insert(Sprite {
+                image: asset_server.load(XeluGamepadButton {
+                    gamepad_button,
+                    gamepad_brand: GamepadBrand::XboxSeries,
+                }),
+                custom_size: Some(vec2(100.0, 100.0)),
+                ..default()
+            });
+            return;
+        }
     }
 }
