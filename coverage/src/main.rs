22aae9d14c4d099fd55bdf8d9745e47194981760
jonathandw743 <jonathandw414@gmail.com>
2025-04-05 15:02:04 +0100

    more coverage tests and removing unknown.png

--- coverage/src/main.rs
+++ coverage/src/main.rs
@@ -1,16 +1,23 @@
-use std::collections::HashSet;
 use bevy_asset::AssetPath;
 use bevy_input::{keyboard::KeyCode, mouse::MouseButton};
-use bevy_input_prompts::xelu::{key_code::XeluKeyCode, mouse_button::XeluMouseButton, LightDark, XeluKeyboardAndMouseSettings};
+use bevy_input_prompts::{
+    kenney::{
+        Format, KenneyKeyboardAndMouseSettings, key_code::KenneyKeyCode,
+        mouse_button::KenneyMouseButton,
+    },
+    xelu::{
+        LightDark, XeluKeyboardAndMouseSettings, key_code::XeluKeyCode,
+        mouse_button::XeluMouseButton,
+    },
+};
+use std::collections::HashSet;
 use walkdir::WalkDir;
 
 fn all_file_paths(path: &'static str) -> HashSet<String> {
     let mut all_file_paths = HashSet::new();
-    for entry in WalkDir::new(
-        format!("assets/{}", path),
-    )
-    .into_iter()
-    .filter_map(Result::ok)
+    for entry in WalkDir::new(format!("assets/{}", path))
+        .into_iter()
+        .filter_map(Result::ok)
     {
         if entry.file_type().is_file() {
             all_file_paths.insert(
@@ -53,30 +60,68 @@ fn log(all_file_paths: HashSet<String>, constructed_file_paths: HashSet<String>)
     );
 }
 
-fn xelu_keyboard_and_mouse() {
-    let all_file_paths = all_file_paths("bevy_input_prompts/xelu/Xelu_Free_Controller&Key_Prompts/Keyboard & Mouse/");
+fn xelu_keyboard_and_mouse() -> (HashSet<String>, HashSet<String>) {
+    let all_file_paths = all_file_paths(
+        "bevy_input_prompts/xelu/Xelu_Free_Controller&Key_Prompts/Keyboard & Mouse/",
+    );
     let mut constructed_file_paths = HashSet::new();
     for light_dark in [LightDark::Dark, LightDark::Light] {
         for key_code in KEY_CODES {
-            constructed_file_paths.insert(constructed_file_path(XeluKeyCode {
+            let c = constructed_file_path(XeluKeyCode {
                 key_code,
                 settings: XeluKeyboardAndMouseSettings { light_dark },
-            }));
+            });
+            if !c.contains("not_found") {
+                constructed_file_paths.insert(c);
+            }
         }
-    }
-    for light_dark in [LightDark::Dark, LightDark::Light] {
         for mouse_button in MOUSE_BUTTONS {
-            constructed_file_paths.insert(constructed_file_path(XeluMouseButton {
+            let c = constructed_file_path(XeluMouseButton {
                 mouse_button,
                 settings: XeluKeyboardAndMouseSettings { light_dark },
-            }));
+            });
+            if !c.contains("no_found") {
+                constructed_file_paths.insert(c);
+            }
         }
     }
-    log(all_file_paths, constructed_file_paths);
+    (all_file_paths, constructed_file_paths)
+}
+
+fn kenney_keyboard_and_mouse() -> (HashSet<String>, HashSet<String>) {
+    let all_file_paths =
+        all_file_paths("bevy_input_prompts/kenney/kenney_input-prompts/Keyboard & Mouse/");
+    let mut constructed_file_paths = HashSet::new();
+    for outline in [false, true] {
+        for format in [Format::Default, Format::Double, Format::Vector] {
+            for key_code in KEY_CODES {
+                let c = constructed_file_path(KenneyKeyCode {
+                    key_code,
+                    settings: KenneyKeyboardAndMouseSettings { outline, format },
+                });
+                if !c.contains("not_found") {
+                    constructed_file_paths.insert(c);
+                }
+            }
+            for mouse_button in MOUSE_BUTTONS {
+                let c = constructed_file_path(KenneyMouseButton {
+                    mouse_button,
+                    settings: KenneyKeyboardAndMouseSettings { outline, format },
+                });
+                if !c.contains("not_found") {
+                    constructed_file_paths.insert(c);
+                }
+            }
+        }
+    }
+    (all_file_paths, constructed_file_paths)
 }
 
 fn main() {
-    xelu_keyboard_and_mouse();
+    let (all_file_paths, constructed_file_paths) = xelu_keyboard_and_mouse();
+    log(all_file_paths, constructed_file_paths);
+    let (all_file_paths, constructed_file_paths) = kenney_keyboard_and_mouse();
+    log(all_file_paths, constructed_file_paths);
 }
 
 const MOUSE_BUTTONS: [MouseButton; 5] = [
