970deea6083c43de92daade68056631aa650b0ab
jonathandw743 <jonathandw414@gmail.com>
2025-04-22 13:13:39 +0100

    predictions not working

--- src/lib.rs
+++ src/lib.rs
@@ -67,14 +67,14 @@ use fs_extra::dir::CopyOptions;
 pub mod directory_representation {
     // bevy_input_prompts_macros::directory_representation!("assets/bevy_input_prompts/not_found/MouseButton");
     // bevy_input_prompts_macros::directory_representation!("assets/test");
-    bevy_input_prompts_macros::directory_representation!("assets/bevy_input_prompts");
+    // bevy_input_prompts_macros::directory_representation!("assets/bevy_input_prompts");
     // bevy_input_prompts_macros::directory_representation!("assets/bevy_input_prompts/kenney/kenney_input-prompts/Xbox Series/Default");
     // bevy_input_prompts_macros::directory_representation!("assets/bevy_input_prompts/xelu/Xelu_Free_Controller&Key_Prompts/Xbox Series");
     // bevy_input_prompts_macros::directory_representation!("assets/bevy_input_prompts/kenney/kenney_input-prompts");
     // bevy_input_prompts_macros::directory_representation!("assets/bevy_input_prompts/kenney/kenney_input-prompts/Flairs/Default");
-    // bevy_input_prompts_macros::directory_representation!(
-    //     "assets/bevy_input_prompts/kenney/kenney_input-prompts/Keyboard & Mouse/Default"
-    // );
+    bevy_input_prompts_macros::directory_representation!(
+        "assets/bevy_input_prompts/kenney/kenney_input-prompts/Keyboard & Mouse/Default"
+    );
     // bevy_input_prompts_macros::directory_representation!("assets/test");
 }
 
